72bc35fea0eaca82d04e19e6e4a94555ff28ed52
sakakiChan <tasakaki2005@yahoo.co.jp>
2024-01-26 12:42:29 +0900

    add test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
+import fortunereturn from '@components/fortunreturn'
 import Footer from '@components/Footer'
 
 export default function Home() {
