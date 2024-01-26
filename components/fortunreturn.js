92be3ea63bd5e87ad3047a6032046b1c4831c056
sakakiChan <tasakaki2005@yahoo.co.jp>
2024-01-26 14:32:30 +0900

    test

--- components/fortunreturn.js
+++ components/fortunreturn.js
@@ -3,17 +3,17 @@ import https from 'https';
 export default function FeedbackForm() {
     const url = "http://api.jugemkey.jp/api/horoscope/free/2024/01/25"
 
-    return <p>test</p>
-        // https.get(url,(res) => {
-        //     let resdata = '';
-        //     res.on("data",function(d){
-        //         resdata += d;
-        //     });
-        //     res.on('end',fanction(){
-        //         resdata;
-        //     })
-        // })
-        // .on("error",(e)=> {
-        //         e;
-        // });
+    https.get(url,(res) => {
+        let resdata = '';
+        res.on("data",function(d){
+            resdata += d;
+        });
+        res.on('end',fanction(){
+            resdata;
+        })
+    })
+    .on("error",(e)=> {
+             e;
+    });
+    return <p>{resdata}</p>
 }
