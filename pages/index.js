7364fa10a02a5c2fe544353d1f8aa1fafcfe49d7
sakakiChan <tasakaki2005@yahoo.co.jp>
2024-01-26 16:57:06 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function constellation(data) {
-  return (
-    <div className="wrapper">
-      <h1>星座一覧を表示</h1>
-         return(
-            <p>{data.horoscope}</p>
-         )
-    </div>
-  )
-}
-
 export default function Home() {
   return (
     <div className="container">
@@ -27,15 +16,16 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-      <constellation/>
+      <h1>星座一覧を表示</h1>
+      <p>{constellations}</p>
       <Footer />
     </div>
   )
 }
   
-export const getStaticProps = async () => {
+export const constellations = async () => {
   const res=await fetch("http://api.jugemkey.jp/api/horoscope/free/2024/01/25")
-  const data=await res.json()
+  const constellations=await res.json()
   
   return {
     props:{constellations}
